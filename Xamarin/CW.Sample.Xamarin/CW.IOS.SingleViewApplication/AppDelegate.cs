fad35997b6733a2fa6efcd659fcd5230706b5f85
Casper Wollesen <CasperWollesen@GMail.com>
2013-11-27 23:01:46 +0100

    Xamarin Sample applications

---
+++ Xamarin/CW.Sample.Xamarin/CW.IOS.SingleViewApplication/AppDelegate.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace CW.IOS.SingleViewApplication
+{
+	// The UIApplicationDelegate for the application. This class is responsible for launching the
+	// User Interface of the application, as well as listening (and optionally responding) to
+	// application events from iOS.
+	[Register ("AppDelegate")]
+	public partial class AppDelegate : UIApplicationDelegate
+	{
+		// class-level declarations
+		UIWindow window;
+		CW_IOS_SingleViewApplicationViewController viewController;
+		//
+		// This method is invoked when the application has loaded and is ready to run. In this
+		// method you should instantiate the window, load the UI into it and then make the window
+		// visible.
+		//
+		// You have 17 seconds to return from this method, or iOS will terminate your application.
+		//
+		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
+		{
+			window = new UIWindow (UIScreen.MainScreen.Bounds);
+			
+			viewController = new CW_IOS_SingleViewApplicationViewController ();
+			window.RootViewController = viewController;
+			window.MakeKeyAndVisible ();
+
+			// check for a notification
+			if (options != null)
+			{
+				// check for a local notification
+				if (options.ContainsKey(UIApplication.LaunchOptionsLocalNotificationKey))
+				{
+					var localNotification = options[UIApplication.LaunchOptionsLocalNotificationKey] as UILocalNotification;
+					if (localNotification != null)
+					{
+						new UIAlertView(localNotification.AlertAction, localNotification.AlertBody, null, "OK", null).Show();
+						// reset our badge
+						UIApplication.SharedApplication.ApplicationIconBadgeNumber = 0;
+					}
+				}
+			}
+
+			return true;
+		}
+
+		public override void ReceivedLocalNotification(UIApplication application, UILocalNotification notification)
+		{
+			// show an alert
+			new UIAlertView(notification.AlertAction, notification.AlertBody, null, "OK", null).Show();
+
+			// reset our badge
+			UIApplication.SharedApplication.ApplicationIconBadgeNumber = 0;
+		}
+	}
+}
+
---
+++ Xamarin/CW.Sample.Xamarin/CW.IOS.SingleViewApplication/CW_IOS_SingleViewApplicationViewController.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Drawing;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace CW.IOS.SingleViewApplication
+{
+	public partial class CW_IOS_SingleViewApplicationViewController : UIViewController
+	{
+		static bool UserInterfaceIdiomIsPhone {
+			get { return UIDevice.CurrentDevice.UserInterfaceIdiom == UIUserInterfaceIdiom.Phone; }
+		}
+
+		public CW_IOS_SingleViewApplicationViewController ()
+			: base (UserInterfaceIdiomIsPhone ? "CW_IOS_SingleViewApplicationViewController_iPhone" : "CW_IOS_SingleViewApplicationViewController_iPad", null)
+		{
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			// Releases the view if it doesn't have a superview.
+			base.DidReceiveMemoryWarning ();
+			
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		int _counter;
+		System.Threading.Thread _thread;
+		void ActionClickOnMeTouchDown (object sender, EventArgs e)
+		{
+			if (_thread == null) {
+				ThreadStart ();
+			} else {
+				ThreadStop ();
+			}
+		}
+
+		void ThreadStop()
+		{
+			_threadActive = false;
+			_thread = null;
+		}
+
+		void ThreadStart()
+		{
+			_thread = new System.Threading.Thread (ActionThread);
+			_thread.IsBackground = true;
+			_thread.Start ();
+		}
+
+		bool _threadActive;
+		void ActionThread()
+		{
+			_threadActive = true;
+			while (_threadActive) {
+
+				InvokeOnMainThread (() => {
+						_counter++;
+						_clickOnMeButton.SetTitle (_counter.ToString(), UIControlState.Normal);
+					});
+
+				System.Threading.Thread.Sleep (1000);
+			}
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			
+			// Perform any additional setup after loading the view, typically from a nib.
+			_clickOnMeButton.TouchDown += ActionClickOnMeTouchDown;
+			_notificationButton.TouchDown += ActionNotificationTouchDown;
+		}
+
+		int _notificationCount;
+		void ActionNotificationTouchDown (object sender, EventArgs e)
+		{
+			int seconds;
+			if(int.TryParse(_notificationTextField.Text, out seconds))
+			{
+				// create the notification
+				var notification = new UILocalNotification();
+
+				// set the fire date (the date time in which it will fire)
+				notification.FireDate = DateTime.Now.AddSeconds(seconds);
+
+				// configure the alert stuff
+				notification.AlertAction = "Time for question";
+				notification.AlertBody = _notificationMesssageTextField.Text + "(" + ++_notificationCount + ")";
+
+				// modify the badge
+				notification.ApplicationIconBadgeNumber = 1;
+
+				// set the sound to be the default sound
+				notification.SoundName = UILocalNotification.DefaultSoundName;
+
+				// schedule it
+				UIApplication.SharedApplication.ScheduleLocalNotification(notification);
+
+				_notificationButton.SetTitle("Success (" + _notificationCount + ")", UIControlState.Normal);
+			}
+			else
+			{
+				_notificationButton.SetTitle ("Failed", UIControlState.Normal);
+			}
+		}
+	}
+}
+
